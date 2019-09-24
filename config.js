6a1f5dd206219bf238cf5512f05038afc25875a3
Krzysztof Branecki <krzysztof@branecki.net>
2019-09-24 15:26:41 +0200

    Add google auth to login page

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GOOGLE_CLIENT_ID:
+    "940937296169-jbicvbis5q5bafdmap3u85ldjopk2lbh.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "5MKhU5yacNB6pw7FoQLuG1XG",
+  CAllBACK_URL: "http://localhost:3000/auth/google/callback"
+};
--- index.js
+++ index.js
@@ -1,20 +1,67 @@
 const express = require("express"),
-  bodyParser = require("body-parser");
-const { check, validationResult } = require("express-validator");
-const { sanitizeBody } = require("express-validator");
+  passport = require("passport"),
+  GoogleStrategy = require("passport-google-oauth2").Strategy,
+  config = require("./config"),
+  app = express();
+let googleProfile = {};
 
-const app = express();
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: config.GOOGLE_CLIENT_ID,
+      clientSecret: config.GOOGLE_CLIENT_SECRET,
+      callbackURL: config.CAllBACK_URL
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      googleProfile = {
+        id: profile.id,
+        displayName: profile.displayName
+      };
+      cb(null, profile);
+    }
+  )
+);
 
 app.set("view engine", "pug");
 app.set("views", "./views");
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get("/", function(req, res) {
-  res.render("index");
+  res.render("index", { user: req.user });
 });
 
-app.post("/auth/google", function(req, res) {
-  res.render("authgoogle");
+app.get("/logged", function(req, res) {
+  res.render("logged", { user: googleProfile });
 });
+
+app.get("/error", function(req, res) {
+  res.render("error");
+});
+
+//Passport routes
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/logged",
+    failureRedirect: "/error"
+  })
+);
 
 app.listen(3000);
 app.use(function(req, res, next) {
